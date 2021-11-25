524dbbc53d1887b97badc56473abbbc2bb2b00d0
Masahiro Takahashi <masezou@gmail.com>
2021-11-25 05:48:16 +0000

    openebs included

--- 4-csi-storage.sh
+++ 4-csi-storage.sh
@@ -57,7 +57,56 @@ BASEPWD=`pwd`
 # Device /dev/sdb check
 if [  -b /dev/sdb ]; then
 echo "openebs installing..."
-./experimental/openebs.sh
+apt install -y open-iscsi
+systemctl enable iscsid && systemctl start iscsid
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+for WORKERNODES in `kubectl get node |grep -v NAME| grep worker | cut -d " " -f 1`; do
+echo ${WORKERNODES}
+kubectl label nodes ${WORKERNODES} node=openebs
+done
+helm repo add openebs https://openebs.github.io/charts
+helm repo update
+helm install openebs --namespace openebs openebs/openebs --create-namespace --set cstor.enabled=true
+echo "Initial wait 30s"
+sleep 30
+while [ "$(kubectl -n openebs get pod openebs-cstor-csi-controller-0 --output="jsonpath={.status.conditions[*].status}" | cut -d' ' -f3)" != "True" ]; do
+        echo "Deploying OpenEBS Please wait...."
+        sleep 30
+done
+WORKERNODES=`kubectl get bd -n openebs | grep -i Unclaimed | cut -d " " -f4`
+BLOCKDEVICENAME=`kubectl get bd -n openebs | grep -i Unclaimed | cut -d " " -f1`
+cat <<EOF | kubectl create -f -
+apiVersion: cstor.openebs.io/v1
+kind: CStorPoolCluster
+metadata:
+ name: cstor-disk-pool
+ namespace: openebs
+spec:
+ pools:
+   - nodeSelector:
+       kubernetes.io/hostname: "${WORKERNODES}"
+     dataRaidGroups:
+       - blockDevices:
+           - blockDeviceName: "${BLOCKDEVICENAME}"
+     poolConfig:
+       dataRaidGroupType: "stripe"
+EOF
+kubectl -n openebs get bd
+cat <<EOF | kubectl create -f -
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: cstor-csi-disk
+provisioner: cstor.csi.openebs.io
+allowVolumeExpansion: true
+parameters:
+  cas-type: cstor
+  # cstorPoolCluster should have the name of the CSPC
+  cstorPoolCluster: cstor-disk-pool
+  # replicaCount should be <= no. of CSPI created in the selected CSPC
+  replicaCount: "1"
+EOF
 else
 echo "hostpath installing..."
 # Rancher local driver (Not CSI Storage)
