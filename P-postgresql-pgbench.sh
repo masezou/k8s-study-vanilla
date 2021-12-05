60076525d3537d129fdf4e13edff33cddba7652f
Masahiro Takahashi <masezou@gmail.com>
2021-12-05 03:45:15 +0000

    removed due to move to installation script

--- P-postgresql-pgbench.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-PGNAMESPACE=postgresql-lb
-
-export POSTGRES_PASSWORD=$(kubectl get secret --namespace ${PGNAMESPACE} postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
-kubectl port-forward --namespace postgresql-lb svc/postgres-postgresql 5432:5432 &
-PGPASSWORD="$POSTGRES_PASSWORD" createdb --host 127.0.0.1 -U postgres pgbenchdb
-PGPASSWORD="$POSTGRES_PASSWORD" pgbench --host 127.0.0.1 -U postgres  -i pgbenchdb
-PGPASSWORD="$POSTGRES_PASSWORD" pgbench --host 127.0.0.1 -U postgres  -c 10 -t 1000  pgbenchdb
