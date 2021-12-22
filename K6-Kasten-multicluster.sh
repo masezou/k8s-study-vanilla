9bae0a4924a79f7f4d2d8899f13523148fb032a6
Masahiro Takahashi <masezou@gmail.com>
2021-12-22 01:56:18 +0000

    added rolebinding

--- K6-Kasten-multicluster.sh
+++ K6-Kasten-multicluster.sh
@@ -3,6 +3,20 @@
 CONTEXT=`kubectl config get-contexts -o=name`
 CLUSTERNAME=`kubectl config get-clusters | tail -n 1`
 k10multicluster setup-primary --context=${CONTEXT} --name=${CLUSTERNAME} 
-
+cat <<EOF | kubectl apply -f -
+apiVersion: auth.kio.kasten.io/v1alpha1
+kind: K10ClusterRoleBinding
+metadata:
+  name: admin-all-clusters
+  namespace: kasten-io-mc
+spec:
+  clusters:
+  - selector: ""
+  k10ClusterRole: k10-multi-cluster-admin
+  subjects:
+  - apiGroup: rbac.authorization.k8s.io
+    kind: User
+    name: system:serviceaccount:kasten-io:k10-k10
+EOF
 
 chmod -x $0
