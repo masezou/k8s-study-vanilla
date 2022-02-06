92852153f162388430f16192831743f80e5865f0
Masahiro Takahashi <masezou@gmail.com>
2022-02-06 09:11:39 +0000

    comment out

--- K6-Kasten-multicluster.sh
+++ K6-Kasten-multicluster.sh
@@ -10,21 +10,24 @@ fi
 CONTEXT=`kubectl config get-contexts -o=name`
 CLUSTERNAME=`kubectl config get-clusters | tail -n 1`
 k10multicluster setup-primary --context=${CONTEXT} --name=${CLUSTERNAME} 
-cat <<EOF | kubectl apply -f -
-apiVersion: auth.kio.kasten.io/v1alpha1
-kind: K10ClusterRoleBinding
-metadata:
-  name: admin-all-clusters
-  namespace: kasten-io-mc
-spec:
-  clusters:
-  - selector: ""
-  k10ClusterRole: k10-multi-cluster-admin
-  subjects:
-  - apiGroup: rbac.authorization.k8s.io
-    kind: User
-    name: system:serviceaccount:kasten-io:k10-k10
-EOF
+
+# 4.5.7 fix
+#cat <<EOF | kubectl apply -f -
+#apiVersion: auth.kio.kasten.io/v1alpha1
+#kind: K10ClusterRoleBinding
+#metadata:
+#  name: admin-all-clusters
+#  namespace: kasten-io-mc
+#spec:
+#  clusters:
+#  - selector: ""
+#  k10ClusterRole: k10-multi-cluster-admin
+#  subjects:
+#  - apiGroup: rbac.authorization.k8s.io
+#    kind: User
+#    name: system:serviceaccount:kasten-io:k10-k10
+#EOF
+
 cat << EOF  | kubectl apply -f -
 apiVersion: v1
 kind: ServiceAccount
