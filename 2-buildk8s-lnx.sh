dabf61c4a8eea3a2329c7468ab726e94d97bbc4f
Masahiro Takahashi <masezou@gmail.com>
2022-03-12 01:23:55 +0000

    added uidmap

--- 2-buildk8s-lnx.sh
+++ 2-buildk8s-lnx.sh
@@ -147,6 +147,7 @@ apt -y purge docker docker.io docker-ce-cli docker-ce docker-ce-rootless-extras
 apt -y install containerd.io
 curl --retry 10 --retry-delay 3 --retry-connrefused -sS https://raw.githubusercontent.com/containerd/containerd/v1.5.10/contrib/autocomplete/ctr -o /etc/bash_completion.d/ctr
 if [ ! -f /usr/local/bin/nerdctl ]; then
+apt -y install uidmap
 NERDCTLVER=0.17.1
 curl --retry 10 --retry-delay 3 --retry-connrefused -sSOL https://github.com/containerd/nerdctl/releases/download/v${NERDCTLVER}/nerdctl-full-${NERDCTLVER}-linux-${ARCH}.tar.gz
 tar xfz nerdctl-full-${NERDCTLVER}-linux-${ARCH}.tar.gz -C /usr/local
