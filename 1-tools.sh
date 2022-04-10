f96a92afc9b688705f0957f712de59d4895b0fc2
Masahiro Takahashi <masezou@gmail.com>
2022-04-10 14:08:57 +0000

    fix typo

--- 1-tools.sh
+++ 1-tools.sh
@@ -124,7 +124,7 @@ fi
 
 # Install trivy 
 #apt -y  install wget apt-transport-https gnupg lsb-release
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | pt-key add -
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list.d/trivy.list
 apt update
 apt -y install trivy
