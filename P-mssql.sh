3ba96a9112cea2d876782c1724798f5ee45309a7
Masahiro Takahashi <masezou@gmail.com>
2022-04-11 01:52:49 +0000

    Importing testdb

--- P-mssql.sh
+++ P-mssql.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
+#########################################################
+MSSQLNAMESPACE=sqlserver
+MSQSQLPASSWORD="MyC0m9l&xP@ssw0rd"
+
+SAMPLEDATA=1
 
 #########################################################
-kubectl create ns sqlserver
-kubectl create secret generic mssql --from-literal=SA_PASSWORD="MyC0m9l&xP@ssw0rd" -n sqlserver
+kubectl create ns ${MSSQLNAMESPACE}
+kubectl create secret generic mssql --from-literal=SA_PASSWORD=${MSQSQLPASSWORD} -n ${MSSQLNAMESPACE}
 cat <<EOF | kubectl create -f -
  kind: PersistentVolumeClaim
  apiVersion: v1
  metadata:
     name: mssql-data
-    namespace: sqlserver
+    namespace: ${MSSQLNAMESPACE}
  spec:
     accessModes:
     - ReadWriteOnce
@@ -21,7 +26,7 @@ cat <<EOF | kubectl create -f -
  kind: Deployment
  metadata:
     name: mssql-deployment
-    namespace: sqlserver
+    namespace: ${MSSQLNAMESPACE}
     labels:
        app: mssql
  spec:
@@ -66,7 +71,7 @@ cat <<EOF | kubectl create -f -
  kind: Service
  metadata:
     name: mssql-deployment
-    namespace: sqlserver
+    namespace: ${MSSQLNAMESPACE}
  spec:
     selector:
        app: mssql
@@ -77,3 +82,27 @@ cat <<EOF | kubectl create -f -
     type: LoadBalancer
 EOF
 
+kubectl -n ${MSSQLNAMESPACE} wait pod -l app=mssql --for condition=Ready --timeout 180s
+EXTERNALIP=`kubectl -n ${MSSQLNAMESPACE} get service mssql-deployment | awk '{print $4}' | tail -n 1`
+DNSDOMAINNAME=`kubectl -n external-dns get deployments.apps  --output="jsonpath={.items[*].spec.template.spec.containers }" | jq |grep rfc2136-zone | cut -d "=" -f 2 | cut -d "\"" -f 1`
+kubectl -n ${MSSQLNAMESPACE} annotate service mssql-deployment \
+    external-dns.alpha.kubernetes.io/hostname=${MSSQLNAMESPACE}.${DNSDOMAINNAME}
+
+if [ ${SAMPLEDATA} -eq 1 ]; then
+sleep 5
+kubectl exec $(kubectl -n${MSSQLNAMESPACE} get pod -l app=mssql -o custom-columns=:metadata.name) -n ${MSSQLNAMESPACE}  -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak
+kubectl exec $(kubectl -n${MSSQLNAMESPACE} get pod -l app=mssql -o custom-columns=:metadata.name) -n ${MSSQLNAMESPACE}  -- /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${MSQSQLPASSWORD}  -Q "RESTORE DATABASE AdventureWorks2019 FROM  DISK = N'/var/opt/mssql/data/AdventureWorks2019.bak' WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf', MOVE 'AdventureWorks2017_Log' TO '/var/opt/mssql/data/AdventureWorks2019_Log.ldf'"
+
+kubectl exec $(kubectl -n${MSSQLNAMESPACE} get pod -l app=mssql -o custom-columns=:metadata.name) -n ${MSSQLNAMESPACE}  -- /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${MSQSQLPASSWORD}  -Q "ALTER DATABASE AdventureWorks2019 SET recovery FULL SELECT CONVERT(nvarchar(50), DATABASEPROPERTYEX('AdventureWorks2019', 'recovery')) AS recovery"
+fi
+
+echo ""
+echo "*************************************************************************************"
+echo "IP address: ${EXTERNALIP}"
+echo "hostname: ${MSSQLNAMESPACE}.${DNSDOMAINNAME}"
+echo "login credential with SA"
+echo " sa / ${MSQSQLPASSWORD}"
+if [ ${SAMPLEDATA} -eq 1 ]; then
+echo ""
+echo "Database: AdventureWorks2019 was imported"
+fi
