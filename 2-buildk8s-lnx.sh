150583da152aed9e74d247173efd65ed1ae0d9da
Masahiro Takahashi <masezou@gmail.com>
2022-04-11 12:04:55 +0000

    MaridDB added

--- 2-buildk8s-lnx.sh
+++ 2-buildk8s-lnx.sh
@@ -262,6 +262,12 @@ ctr images push --platform linux/${ARCH} --plain-http ${REGISTRY}/bitnami/mysql:
 ctr images rm docker.io/bitnami/mysql:8.0.28-debian-10-r63
 ctr images rm ${REGISTRY}/bitnami/mysql:8.0.28-debian-10-r63
 
+ctr images pull --platform linux/${ARCH} docker.io/bitnami/mariadb:10.5.15-debian-10-r52
+ctr images tag docker.io/bitnami/mariadb:10.5.15-debian-10-r52 ${REGISTRY}/bitnami/mariadb:10.5.15-debian-10-r52
+ctr images push --platform linux/${ARCH} --plain-http ${REGISTRY}/bitnami/mariadb:10.5.15-debian-10-r52
+ctr images rm docker.io/bitnami/mariadb:10.5.15-debian-10-r52
+ctr images rm ${REGISTRY}/bitnami/mariadb:10.5.15-debian-10-r52
+
 ctr images pull --platform linux/${ARCH} docker.io/bitnami/postgresql:14.2.0-debian-10-r58
 ctr images tag docker.io/bitnami/postgresql:14.2.0-debian-10-r58 ${REGISTRY}/bitnami/postgresql:14.2.0-debian-10-r58
 ctr images push --platform linux/${ARCH} --plain-http ${REGISTRY}/bitnami/postgresql:14.2.0-debian-10-r58
---
+++ P-mariadb.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+#########################################################
+# Force Online Install
+#FORCE_ONLINE=1
+
+MYSQL_NAMESPACE=maria-db
+# SC = csi-hostpath-sc / local-path / nfs-csi / vsphere-sc / example-vanilla-rwo-filesystem-sc / cstor-csi-disk
+SC=vsphere-sc
+
+SAMPLEDATA=0
+
+#REGISTRYURL=192.168.133.2:5000
+
+#########################################################
+if [ -z ${REGISTRYURL} ]; then
+REGISTRYHOST=`kubectl -n registry get configmaps pregistry-configmap -o=jsonpath='{.data.pregistry_host}'`
+REIGSTRYPORT=`kubectl -n registry get configmaps pregistry-configmap -o=jsonpath='{.data.pregistry_port}'`
+REGISTRYURL=${REGISTRYHOST}:${REIGSTRYPORT}
+curl -s  -X GET http://${REGISTRYURL}/v2/_catalog |grep mariadb
+retvalcheck=$?
+if [ ${retvalcheck} -eq 0 ]; then
+  ONLINE=0
+  else
+  ONLINE=1
+fi
+if [ ! -z ${FORCE_ONLINE} ] ; then
+ONLINE=1
+fi
+fi
+
+### Install command check ####
+if type "kubectl" > /dev/null 2>&1
+then
+    echo "kubectl was already installed"
+else
+    echo "kubectl was not found. Please install kubectl and re-run"
+    exit 255
+fi
+
+if type "helm" > /dev/null 2>&1
+then
+    echo "helm was already installed"
+else
+if [ ! -f /usr/local/bin/helm ]; then
+curl -s -O https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+bash ./get-helm-3
+helm version
+rm get-helm-3
+helm completion bash > /etc/bash_completion.d/helm
+source /etc/bash_completion.d/helm
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+fi
+fi
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+kubectl create namespace ${MYSQL_NAMESPACE}
+if [ ${ONLINE} -eq 0 ]; then
+helm fetch bitnami/mariadb 
+MYSQLCHART=`ls mariadb-10.5.0.tgz`
+if [ ${SC} = csi-hostpath-sc ]; then
+helm install --namespace ${MYSQL_NAMESPACE} mariadb-release ${MYSQLCHART} --set primary.service.type=LoadBalancer --set global.storageClass=${SC} --set volumePermissions.enabled=true --set global.imageRegistry=${REGISTRYURL}
+else
+helm install --namespace ${MYSQL_NAMESPACE} mariadb-release ${MYSQLCHART} --set primary.service.type=LoadBalancer --set global.storageClass=${SC} --set global.imageRegistry=${REGISTRYURL}
+fi
+else
+if [ ${SC} = csi-hostpath-sc ]; then
+helm install --namespace ${MYSQL_NAMESPACE} mariadb-release bitnami/mariadb --set primary.service.type=LoadBalancer --set global.storageClass=${SC} --set volumePermissions.enabled=tru
+else
+helm install --namespace ${MYSQL_NAMESPACE} mariadb-release bitnami/mariadb --set primary.service.type=LoadBalancer --set global.storageClass=${SC}
+fi
+fi
+
+sleep 5
+kubectl -n ${MYSQL_NAMESPACE} get pod,pvc
+while [ "$(kubectl -n ${MYSQL_NAMESPACE} get pod mariadb-release-0 --output="jsonpath={.status.containerStatuses[*].ready}" | cut -d' ' -f2)" != "true" ]; do
+        echo "Deploying MariaDB, Please wait...."
+    kubectl get pod,pvc -n ${MYSQL_NAMESPACE}
+        sleep 30
+done
+    kubectl get pod,pvc -n ${MYSQL_NAMESPACE}
+sleep 5
+
+
+kubectl -n ${MYSQL_NAMESPACE} wait pod -l app.kubernetes\.io\/name=mariadb --for condition=Ready
+
+EXTERNALIP=`kubectl -n ${MYSQL_NAMESPACE} get svc mariadb-release | awk '{print $4}' | tail -n 1`
+echo $EXTERNALIP
+DNSDOMAINNAME=`kubectl -n external-dns get deployments.apps  --output="jsonpath={.items[*].spec.template.spec.containers }" | jq |grep rfc2136-zone | cut -d "=" -f 2 | cut -d "\"" -f 1`
+kubectl -n ${MYSQL_NAMESPACE} annotate service  mariadb-release  \
+    external-dns.alpha.kubernetes.io/hostname=${MYSQL_NAMESPACE}.${DNSDOMAINNAME}
+kubectl images -n ${MYSQL_NAMESPACE}
+echo ""
+echo "*************************************************************************************"
+MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace ${MYSQL_NAMESPACE} mariadb-release -o jsonpath="{.data.mariadb-root-password}" | base64 --decode)
+echo ""
+echo "MySQL Host: ${MYSQL_NAMESPACE}.${DNSDOMAINNAME} / ${EXTERNALIP}"
+echo "Credential: root / ${MYSQL_ROOT_PASSWORD}"
+echo ""
+echo "How to connect"
+echo -n 'MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace'
+echo -n " ${MYSQL_NAMESPACE} "
+echo -n 'mariadb-release -o jsonpath="{.data.mariadb-root-password}" | base64 --decode)'
+echo ""
+echo -n "mysql -h $EXTERNALIP -uroot -p"
+echo -n '"$MYSQL_ROOT_PASSWORD"'
+echo ""
+
