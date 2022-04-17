65ea35c33ab627359370d5517c1ae3a19aec3fef
Masahiro Takahashi <masezou@gmail.com>
2022-04-17 09:12:34 +0000

    fixed minor bug

--- P-mssql.sh
+++ P-mssql.sh
@@ -101,9 +101,9 @@ cat <<EOF | kubectl create -f -
       targetPort: 1433
     type: LoadBalancer
 EOF
+kubectl -n ${MSSQLNAMESPACE} wait pod -l app=mssql --for condition=Ready --timeout 180s
 fi
 
-kubectl -n ${MSSQLNAMESPACE} wait pod -l app=mssql --for condition=Ready --timeout 180s
 EXTERNALIP=`kubectl -n ${MSSQLNAMESPACE} get service mssql-deployment | awk '{print $4}' | tail -n 1`
 DNSDOMAINNAME=`kubectl -n external-dns get deployments.apps  --output="jsonpath={.items[*].spec.template.spec.containers }" | jq |grep rfc2136-zone | cut -d "=" -f 2 | cut -d "\"" -f 1`
 if [ ${retvalsvc} -ne 0 ]; then
