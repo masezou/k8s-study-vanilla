e0e3fc40e9d7ee223a7433222acf2a68247e3800
Masahiro Takahashi <masezou@gmail.com>
2022-04-23 06:30:00 +0000

    apt config change

--- 1-tools.sh
+++ 1-tools.sh
@@ -350,8 +350,11 @@ apt -y install apt-transport-https ca-certificates curl gnupg-agent software-pro
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 cat << EOF > /etc/apt/apt.conf.d/90_no_prompt
-APT::Get::Assume-Yes "true";
-APT::Get::force-yes "true";
+APT {
+    Get {
+        force-yes "true";
+    };
+};
 EOF
 if [ ${ARCH} = amd64 ]; then
   add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
