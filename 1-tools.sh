d821d6e239940af29530b9bc71ec37c089c81d9a
Masahiro Takahashi <masezou@gmail.com>
2022-05-05 17:06:59 +0000

    small fix

--- 1-tools.sh
+++ 1-tools.sh
@@ -140,7 +140,7 @@ kubectl completion bash >/etc/bash_completion.d/kubectl
 source /etc/bash_completion.d/kubectl
 echo 'export KUBE_EDITOR=vi' >>~/.bashrc
 if [ ! -f /usr/local/bin/kubectl-convert ]; then
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${ARCH}/kubectl-convert"
+curl --retry 10 --retry-delay 3 --retry-connrefused -sSOL "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${ARCH}/kubectl-convert"
 install -o root -g root -m 0755 kubectl-convert /usr/local/bin/kubectl-convert
 rm ./kubectl-convert
 fi
