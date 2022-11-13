ceea18b60226b46b8b3aa13b4322668ada1c0e4a
Masahiro Takahashi <masezou@gmail.com>
2022-11-13 05:52:07 +0000

    removed indivisual govc setting

--- 5-csi-vsphere.sh
+++ 5-csi-vsphere.sh
@@ -326,9 +326,7 @@ if [ -f K3-kasten-vsphere.sh ]; then
 		rm -rf vsphere-env
 		mkdir -p /home/${SUDO_USER}/k8s-study-vanilla/
 		cp K3-kasten-vsphere.sh /home/${SUDO_USER}/k8s-study-vanilla/K3-kasten-vsphere.sh
-		cp ~/govc-vcenter.sh /home/${SUDO_USER}/
 		chown ${SUDO_USER}:${SUDO_USER} /home/${SUDO_USER}/k8s-study-vanilla/K3-kasten-vsphere.sh
-		chown ${SUDO_USER}:${SUDO_USER} /home/${SUDO_USER}/govc-vcenter.sh
 	fi
 fi
 # Remove taint
