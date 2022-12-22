75d1e689a291313560a8c3a91410ee2483ea63d5
Masahiro Takahashi <masezou@gmail.com>
2022-12-22 23:28:22 +0000

    fix bug

--- result.sh
+++ result.sh
@@ -208,7 +208,7 @@ if [ ${HAS_KASTEN} -eq 0 ]; then
 	if [ $KASTEN125 -eq 1 ]; then
 		desired_token_secret_name=k10-k10-token
 		kubectl get secret ${desired_token_secret_name} --namespace kasten-io -ojsonpath="{.data.token}" | base64 --decode >k10-k10.token
-		echo >k10-k10.token
+	    echo "" >>k10-k10.token
 	else
 		sa_secret=$(kubectl get serviceaccount k10-k10 -o jsonpath="{.secrets[0].name}" --namespace kasten-io)
 		kubectl get secret $sa_secret --namespace kasten-io -ojsonpath="{.data.token}{'\n'}" | base64 --decode >k10-k10.token
