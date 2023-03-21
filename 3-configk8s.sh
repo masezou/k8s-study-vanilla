1a52c47505b630d21cfa527e99ca41e63871adfa
Masahiro Takahashi <masezou@gmail.com>
2023-03-21 22:33:13 +0000

    fix external-dns registry location

--- 3-configk8s.sh
+++ 3-configk8s.sh
@@ -547,7 +547,7 @@ spec:
       serviceAccountName: external-dns
       containers:
       - name: external-dns
-        image: k8s.gcr.io/external-dns/external-dns:v0.13.2
+        image:registry.k8s.io/external-dns/external-dns:v0.13.2
         args:
         - --provider=rfc2136
         - --registry=txt
