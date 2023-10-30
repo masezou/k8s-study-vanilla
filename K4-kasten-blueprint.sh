ed4fe8ff3d9cbe3a50d0e64cda5838be111195b3
Masahiro Takahashi <masezou@gmail.com>
2023-10-30 21:48:00 +0000

    fix blueprint detection

--- K4-kasten-blueprint.sh
+++ K4-kasten-blueprint.sh
@@ -6,9 +6,9 @@ KANISTERVER=0.98.0
 
 ##################################################
 
-if [ -z ${KANISTERVER} ]; then
-KANISTERVER=$(kubectl -n kasten-io get deployments.apps catalog-svc -o json | grep kanister-tools | cut -d "/" -f 2 | cut -d ":" -f 2 | cut -d "-" -f 2 | cut -d "\"" -f 1)
-fi
+#if [ -z ${KANISTERVER} ]; then
+#KANISTERVER=$(kubectl -n kasten-io get deployments.apps catalog-svc -o json | grep kanister-tools | cut -d "/" -f 2 | cut -d ":" -f 2 | cut -d "-" -f 2 | cut -d "\"" -f 1)
+#fi
 
 echo "Kanister version: ${KANISTERVER}"
 
@@ -20,59 +20,13 @@ kubectl --namespace kasten-io apply -f \
 kubectl --namespace kasten-io apply -f \
 	https://raw.githubusercontent.com/kanisterio/kanister/${KANISTERVER}/examples/postgresql/blueprint-v2/postgres-blueprint.yaml
 kubectl --namespace kasten-io apply -f \
+    https://raw.githubusercontent.com/kanisterio/kanister/${KANISTERVER}/examples/postgresql-ha/hook-blueprint/postgres-ha-hook.yaml
+kubectl --namespace kasten-io apply -f \
 	https://raw.githubusercontent.com/kanisterio/kanister/${KANISTERVER}/examples/etcd/etcd-in-cluster/k8s/etcd-incluster-blueprint.yaml
 kubectl --namespace kasten-io apply -f \
 	https://raw.githubusercontent.com/kanisterio/kanister/${KANISTERVER}/examples/maria/blueprint-v2/maria-blueprint.yaml
 kubectl --namespace kasten-io apply -f \
 	https://raw.githubusercontent.com/kanisterio/kanister/${KANISTERVER}/examples/mssql/blueprint-v2/mssql-blueprint.yaml
-
-#kubectl --namespace kafka-test apply -f \
-#     https://raw.githubusercontent.com/kanisterio/kanister/${KANISTERVER}/examples/kafka/adobe-s3-connector/kafka-blueprint.yaml
-
-# Application-Consistent Backups
-
-# PostgreSQL
-cat <<'EOF' | kubectl --namespace=kasten-io create -f -
-apiVersion: cr.kanister.io/v1alpha1
-kind: Blueprint
-metadata:
-  name: postgresql-hooks
-actions:
-  backupPrehook:
-    phases:
-    - func: KubeExec
-      name: makePGCheckPoint
-      args:
-        namespace: "{{ .StatefulSet.Namespace }}"
-        pod: "{{ index .StatefulSet.Pods 0 }}"
-        container: postgres-postgresql
-        command:
-        - bash
-        - -o
-        - errexit
-        - -o
-        - pipefail
-        - -c
-        - |
-          PGPASSWORD=${POSTGRES_PASSWORD} psql -U $POSTGRES_USER -c "select pg_start_backup('app_cons');"
-  backupPosthook:
-    phases:
-    - func: KubeExec
-      name: afterPGBackup
-      args:
-        namespace: "{{ .StatefulSet.Namespace }}"
-        pod: "{{ index .StatefulSet.Pods 0 }}"
-        container: postgres-postgresql
-        command:
-        - bash
-        - -o
-        - errexit
-        - -o
-        - pipefail
-        - -c
-        - |
-          PGPASSWORD=${POSTGRES_PASSWORD} psql -U $POSTGRES_USER -c "select pg_stop_backup();"
-EOF
 
 echo "*************************************************************************************"
 echo "Pre-defined blueprints were configured"
