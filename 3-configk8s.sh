e208e7ec1debbef61121452a840a85c502736d76
Masahiro Takahashi <masezou@gmail.com>
2023-11-07 13:21:30 +0000

    revert version

--- 3-configk8s.sh
+++ 3-configk8s.sh
@@ -540,7 +540,7 @@ spec:
       serviceAccountName: external-dns
       containers:
       - name: external-dns
-        image: registry.k8s.io/external-dns/external-dns:v0.14.0
+        image: registry.k8s.io/external-dns/external-dns:v0.13.6
         args:
         - --provider=rfc2136
         - --registry=txt
